b6d677f405d37163456bd5adc40ce72b330edd1f
Odewale Toheeb <toheebtemitope5@gmail.com>
2024-05-08 06:56:56 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-#!/usr/bin/pup
-# Install a specifick version of flask (2.1.0)
-package {'flask':
-	ensure	 => '2.1.0',
-	provider => 'pip'
+#!/usr/bin/pip
+# Install a version of flask (2.1.0)
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
