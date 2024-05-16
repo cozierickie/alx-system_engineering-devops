512f3e9eaaf27ae954f5d45c36bb547300e0b53c
Odewale Toheeb <toheebtemitope5@gmail.com>
2024-05-16 07:53:07 +0000

    using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,15 @@
-# Ensure that password authentication is disabled in SSH client configuration
-file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/sshd_config',
-  line    => 'PasswordAuthentication no',
-  match   => '^#PasswordAuthentication',
-  replace => 'PasswordAuthentication',
-}
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
 
-# Add or update the IdentityFile configuration to use the private key ~/.ssh/school
-file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#IdentityFile',
-  replace => 'IdentityFile',
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentification no
+	",
+
 }
